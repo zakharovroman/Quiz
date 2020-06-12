855ea7841069e4ce5f662014f2787c355e0b1016
Zakharov Roman <zakharovroman@hotmail.com>
2020-06-12 14:49:32 +0300

    Master version from ZakharovRoman

--- Quiz/ViewControlles/ViewController.swift
+++ Quiz/ViewControlles/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("I am Anzhelika!")
+        print("Master version")
     }
 
 
